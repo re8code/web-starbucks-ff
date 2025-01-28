5c6e2d8083ef9a90b058dd08b62b5cc28b4da7ef
re8code <won@re8code.com>
2025-01-28 20:55:48 +0900

    step 12, badges

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgesEl = document.querySelector('header .badges');
+window.addEventListener('scroll', _.throttle(function () {
+  if (window.scrollY > 500) {
+    // badgesEl.style.display = 'none';
+    gsap.to(badgesEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  }
+  else {
+    // badgesEl.style.display = 'block';
+    gsap.to(badgesEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(handler pointer, milli-seconds);
